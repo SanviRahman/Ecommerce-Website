eb010ef4886d69fcc9d316bdf2d0041a32700af7
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-03-30 12:53:35 +0600

    Sanvi

--- core/apps/users/apps.py
+++ core/apps/users/apps.py
@@ -1,6 +1,12 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
+
+# class UsersConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'core.apps.users'
+
+from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'core.apps.users'
---
+++ core/apps/users/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from django.utils import timezone
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('phone_number', 'password', 'password2', 'first_name', 'last_name')
+
+    def validate_phone_number(self, value):
+        if User.objects.filter(phone_number=value).exists():
+            raise serializers.ValidationError("A user with this phone number already exists.")
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
